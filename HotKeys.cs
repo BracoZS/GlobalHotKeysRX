192cb2558f39e66b2433a48c1ff4dfcea2a5d8ae
BracoZS <joseph_124_@hotmail.com>
2024-07-27 17:45:08 -0500

    Class names and variables updated.

---
+++ HotKeys.cs
@@ -0,0 +1,123 @@
+
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+using System.Windows.Input;
+
+namespace GlobalHotKeysRX
+{
+    [Flags]
+    public enum Modifiers
+    {
+        None = 0,
+        Alt = 1,
+        Control = 2,
+        NoRepeat = 0x4000,
+        Shift = 4,
+        Win = 8
+    }
+    public struct Combo
+    {
+        public Modifiers modifier { get; set; }
+        public Key key { get; set; }
+
+        public Combo( Modifiers modifier, Key key )
+        {
+            this.modifier = modifier;
+            this.key = key;
+        }
+        public Combo( Modifiers modifier1, Modifiers modifier2, Key key )
+        {
+            this.modifier = modifier1 | modifier2;
+            this.key = key;
+        }
+    }
+
+    public class HotKeys
+    {
+        #region Windows API
+        const int WM_HOTKEY = 0x0312;
+
+        [DllImport("user32.dll")]
+        static extern bool RegisterHotKey( IntPtr hWnd, int id, uint fsModifiers, uint vk );
+        [DllImport("user32.dll")]
+        static extern bool UnregisterHotKey( IntPtr hWnd, int id );
+        #endregion
+
+        private Dictionary<int, Action> _methods;
+        public HotKeys()
+        {
+            ComponentDispatcher.ThreadFilterMessage += new ThreadMessageEventHandler(OnThreadFilterMessage);
+            _methods = new Dictionary<int, Action>();
+        }
+        public HotKeys(Combo c, Action a):this()     // - :this() - !important
+        {
+            AddMethod(c, a);
+        }
+        private int getID(Combo c)
+        {
+            return (int)c.modifier + (int)c.key;     
+        }
+        public bool AddMethod(Combo combo, Action action)       //implement exception...
+        {
+            var id = getID(combo);
+            var vKeyCode = KeyInterop.VirtualKeyFromKey(combo.key);
+            bool okRegister = RegisterHotKey(IntPtr.Zero, id, (uint)combo.modifier, (uint)vKeyCode);
+
+            if (okRegister) {_methods.Add(id, action);};
+            return okRegister;
+        }
+        public bool DeleteMethod(Combo c)
+        {
+            bool okDelete = false;     
+            var id = getID(c);
+            if (_methods.ContainsKey(id))
+            {
+                okDelete = UnregisterHotKey(IntPtr.Zero, id);
+                _methods.Remove(id);
+            }
+            return okDelete;    
+        }
+        public void ClearAll()
+        {
+            foreach (var item in _methods)
+            {
+                UnregisterHotKey(IntPtr.Zero, item.Key);
+            }
+            _methods.Clear();
+        }
+        private void OnThreadFilterMessage(ref MSG msg, ref bool handled)
+        {
+            if (!handled && msg.message == WM_HOTKEY)
+            {
+                var id = msg.wParam.ToInt32();
+                _methods[id]?.Invoke();     
+
+                handled = true;
+            }
+        }
+
+        #region Dispose
+        bool _disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose( bool disposing )
+        {
+            if(!_disposed)
+            {
+                if(disposing)
+                {
+                    ClearAll();
+                }
+
+                _disposed = true;
+            }
+        }
+        #endregion
+    }
+    
+}
